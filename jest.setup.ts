3e85e2c907377fff9e650b9fa58d2b78ff992d8a
frost drago <nowhere@mail.com>
2026-03-05 11:28:08 +0700

    Week 5

---
+++ jest.setup.ts
@@ -0,0 +1,7 @@
+import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+(globalThis as unknown as { TextEncoder: typeof TextEncoder }).TextEncoder =
+  TextEncoder;
+(globalThis as unknown as { TextDecoder: typeof TextDecoder }).TextDecoder =
+  TextDecoder;
